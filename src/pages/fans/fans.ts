9c1b48d54cb0d0613092f7202cbafaced3b6a679
zhang juan <2376274564@qq.com>
2018-08-01 15:30:14 +0800

    commit -a

--- src/pages/fans/fans.ts
+++ src/pages/fans/fans.ts
@@ -3,6 +3,10 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 import { FandetailPage } from '../fandetail/fandetail'
 
+import { StorageProvider } from '../../providers/storage/storage';
+
+import { HttpServicesProvider } from '../../providers/http-services/http-services';
+
 /**
  * Generated class for the FansPage page.
  *
@@ -17,15 +21,42 @@ import { FandetailPage } from '../fandetail/fandetail'
 })
 export class FansPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  public userInfo='';
+
+  public fans='';
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,public storage:StorageProvider,public httpService:HttpServicesProvider) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad FansPage');
-  }
+  // ionViewDidLoad() {
+  //   console.log('ionViewDidLoad FansPage');
+  // }
     onClick(){
-      
       this.navCtrl.push(FandetailPage);
-     
     }
+    ionViewWillEnter(){
+       let userInfo=this.storage.get('userInfo');
+      //console.log('userInfo');return;
+      let token=this.storage.get('token');
+      console.log("token");
+      if(token){
+        //api请求
+        let api='v1/MemberShip/GetFans/' + token;
+        this.httpService.requestData(api,(data)=>{
+          if(data.error_code==0){
+            //请求成功
+            console.log('1');
+             this.storage.set('data',data.data);//转数组
+             this.fans=data.data;
+             
+        }else {
+          // this.userInfo='';
+          console.log('0');
+        }
+      });
+      }else{
+        this.userInfo='';
+      }
+    }
+  
 }
