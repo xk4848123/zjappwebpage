42c5e89842de3388bffe867b092c07f9c5f0c384
zhang juan <2376274564@qq.com>
2018-08-10 10:49:45 +0800

    commit

--- src/pages/fans/fans.ts
+++ src/pages/fans/fans.ts
@@ -7,6 +7,7 @@ import { StorageProvider } from '../../providers/storage/storage';
 
 import { HttpServicesProvider } from '../../providers/http-services/http-services';
 
+import { AlertProvider } from '../../providers/alert/alert';
 /**
  * Generated class for the FansPage page.
  *
@@ -21,41 +22,56 @@ import { HttpServicesProvider } from '../../providers/http-services/http-service
 })
 export class FansPage {
 
-  public userInfo='';
 
-  public fans='';
+  public fansmessage='';
+  //public temp=[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,public storage:StorageProvider,public httpService:HttpServicesProvider) {
+  public color='';
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,public storage:StorageProvider,public httpService:HttpServicesProvider,public alertProvider: AlertProvider) {
   }
 
   // ionViewDidLoad() {
   //   console.log('ionViewDidLoad FansPage');
   // }
-    onClick(){
-      this.navCtrl.push(FandetailPage);
+    onClick(item:any){
+      
+    }
+    Red(event){
+       this.color=event.target.innerHTML
+       this.navCtrl.push(FandetailPage,{
+        item:this.color
+      });
+       console.log(this.color);
     }
     ionViewWillEnter(){
-       let userInfo=this.storage.get('userInfo');
-      //console.log('userInfo');return;
       let token=this.storage.get('token');
-      console.log("token");
       if(token){
         //api请求
         let api='v1/MemberShip/GetFans/' + token;
         this.httpService.requestData(api,(data)=>{
           if(data.error_code==0){
             //请求成功
-            console.log('1');
-             this.storage.set('data',data.data);//转数组
-             this.fans=data.data;
-             
-        }else {
-          // this.userInfo='';
-          console.log('0');
-        }
+           this.storage.set('fans',data.data);
+            this.fansmessage=data.data;
+            console.log(data.data);
+            //  let temp=JSON.stringify(this.fansmessage);//字符串 
+            //    let tempData=temp.split(',');
+            let arr=[];
+            for(let i in data.data){
+              if(!data.data.hasOwnProperty(i)){
+                   continue;
+              }
+              let temp={};
+             temp[i]=data.data[i];
+                   //arr.push(data.data[i])
+                  arr.push(temp);
+            }
+              console.log(arr);
+            }else{
+           this.alertProvider.showAlert('数据获取异常','',['ok']);
+                 }
       });
-      }else{
-        this.userInfo='';
       }
     }
   
