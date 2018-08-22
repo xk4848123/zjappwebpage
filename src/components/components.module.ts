d23b4925b60f85a75955c20205c84ff25e25628d
wei <xw@zhongjianmall.com>
2018-08-22 09:16:40 +0800

    tyf30

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,6 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CarModalComponent } from './car-modal/car-modal';
-import { ShareComponent } from './share/share';
 @NgModule({
 	declarations: [
     ],
