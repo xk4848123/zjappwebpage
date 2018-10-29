aaeb4611b0c3ac08e905061327fcb7726991a7e0
xk48481 <15395068264@163.com>
2018-10-29 09:32:31 +0800

    commit

--- src/pages/orderlist/orderlist.ts
+++ src/pages/orderlist/orderlist.ts
@@ -25,8 +25,9 @@ import { WeblinkProvider } from '../../providers/weblink/weblink';
 })
 export class OrderlistPage {
 
-  public orderData:(any);
-  constructor(public weblink:WeblinkProvider,public rlogin:RloginprocessProvider,public passwordProvider:VerifypasswordProvider,public navCtrl: NavController, public navParams: NavParams, public storage: StorageProvider, public httpService: HttpServicesProvider, public toast: ToastProvider, private config: ConfigProvider) {
+  public tempData = '';
+
+  constructor(public weblink: WeblinkProvider, public rlogin: RloginprocessProvider, public passwordProvider: VerifypasswordProvider, public navCtrl: NavController, public navParams: NavParams, public storage: StorageProvider, public httpService: HttpServicesProvider, public toast: ToastProvider, private config: ConfigProvider) {
   }
 
 
@@ -38,8 +39,8 @@ export class OrderlistPage {
       let api = 'v1/PersonalCenter/getOrderDetails/' + token + '/' + this.navParams.get('orderId');
       this.httpService.requestData(api, (data) => {
         if (data.error_code == 0) {
-          this.orderData = data.data;
-        } else if(data.error_code == 3){
+           this.tempData = data.data;
+        } else if (data.error_code == 3) {
           //抢登处理
           this.rlogin.rLoginProcess(this.navCtrl);
         }
@@ -51,54 +52,54 @@ export class OrderlistPage {
 
   }
   //申请退款
-  pushrefund(){
+  pushrefund() {
     this.navCtrl.push('RefundPage',
-    {
-      orderNo:this.orderData.orderno
-    });
+      {
+        orderNo: this.tempData['orderno']
+      });
   }
-   //申请退货
-   pushsale(){
+  //申请退货
+  pushsale() {
     this.navCtrl.push('SalereturnPage',
-    {
-      orderNo:this.orderData.orderno
-    });
+      {
+        orderNo: this.tempData['orderno']
+      });
   }
-   //取消订单
-   pushcancelOrder() {
+  //取消订单
+  pushcancelOrder() {
     let token = this.storage.get('token');
     if (token) {
       //api请求
-      let api = 'v1/PersonalCenter/cancelOrder/' +token;
-       //发送请求提交退款申请
-       this.httpService.doFormPost(api,{orderNo:this.orderData.orderno} ,(data) => {
-          if (data.error_code == 0) {
+      let api = 'v1/PersonalCenter/cancelOrder/' + token;
+      //发送请求提交退款申请
+      this.httpService.doFormPost(api, { orderNo: this.tempData['orderno'] }, (data) => {
+        if (data.error_code == 0) {
 
-            //取消订单后
-            this.navCtrl.push('OrderhandletransferPage',{type: '1'});
-         } else if(data.error_code == 3){
-           //抢登处理
-           this.rlogin.rLoginProcess(this.navCtrl);
-         }
-         else {
-           this.toast.showToast(data.error_message);
-         }
+          //取消订单后
+          this.navCtrl.push('OrderhandletransferPage', { type: '1' });
+        } else if (data.error_code == 3) {
+          //抢登处理
+          this.rlogin.rLoginProcess(this.navCtrl);
+        }
+        else {
+          this.toast.showToast(data.error_message);
+        }
       });
     }
   }
   //确认收货
-  confirmorder(){
+  confirmorder() {
     let token = this.storage.get('token');
     if (token) {
       //api请求
-      let api = 'v1/PersonalCenter/confirmOrder/' +token;
+      let api = 'v1/PersonalCenter/confirmOrder/' + token;
       //发送请求提交退款申请
-      this.httpService.doFormPost(api,{orderNo:this.orderData.orderno} ,(data) => {
-          if (data.error_code == 0) {
-           //确认收货后处理
-           this.navCtrl.push('OrderhandletransferPage',{type: '2'})
-        
-        } else if(data.error_code == 3){
+      this.httpService.doFormPost(api, { orderNo: this.tempData['orderno'] }, (data) => {
+        if (data.error_code == 0) {
+          //确认收货后处理
+          this.navCtrl.push('OrderhandletransferPage', { type: '2' })
+
+        } else if (data.error_code == 3) {
           //抢登处理
           this.rlogin.rLoginProcess(this.navCtrl);
         }
@@ -109,45 +110,45 @@ export class OrderlistPage {
     }
   }
   //查看物流
-  information(){
+  information() {
     this.navCtrl.push('InformationPage',
-    {
-    orderNo: this.orderData.orderno
-    });
+      {
+        orderNo: this.tempData['orderno']
+      });
   }
   //立即支付
-  payNow(){
-    var api = "v2/PersonalCenter/HandleEOrder/"+this.storage.get('token');
+  payNow() {
+    var api = "v2/PersonalCenter/HandleEOrder/" + this.storage.get('token');
     var params = {
-      "orderNo":this.orderData.orderno
+      "orderNo": this.tempData['orderno']
     }
-    this.httpService.doFormPost(api,params,(data)=>{
-      if(data.data.type==1){
+    this.httpService.doFormPost(api, params, (data) => {
+      if (data.data.type == 1) {
         //使用虚拟货币未使用钱 
-        this.passwordProvider.execute(this.navCtrl,()=>{
-          var api = "v1/PersonalCenter/syncHandleOrder/"+this.storage.get('token');
+        this.passwordProvider.execute(this.navCtrl, () => {
+          var api = "v1/PersonalCenter/syncHandleOrder/" + this.storage.get('token');
           var params = {
-            "orderNo":data.data.datas
+            "orderNo": data.data.datas
           }
-          this.httpService.doFormPost(api,params,(data)=>{
-            if(data.error_code==0){
-              this.navCtrl.push('PaysuccessPage',{
-                "orderType":"1"
+          this.httpService.doFormPost(api, params, (data) => {
+            if (data.error_code == 0) {
+              this.navCtrl.push('PaysuccessPage', {
+                "orderType": "1"
               });
-            }else if(data.error_code==3){
+            } else if (data.error_code == 3) {
               this.rlogin.rLoginProcess(this.navCtrl);
-            }else{
+            } else {
               this.toast.showToast(data.error_message);
             }
           });
         });
-      }else if(data.data.type==2){
+      } else if (data.data.type == 2) {
         //使用钱
-        this.weblink.wxGoWebPay(this.navCtrl,data.data.datas.orderNo,data.data.datas.realpay,data.data.datas.orderType);
-      }else if(data.data.type==3){
+        this.weblink.wxGoWebPay(this.storage.get('token'), data.data.datas.orderNo, data.data.datas.realpay, data.data.datas.orderType);
+      } else if (data.data.type == 3) {
         //使用虚拟货币使用钱
-        this.passwordProvider.execute(this.navCtrl,()=>{
-          this.weblink.wxGoWebPay(this.navCtrl,data.data.datas.orderNo,data.data.datas.realpay,data.data.datas.orderType);
+        this.passwordProvider.execute(this.navCtrl, () => {
+          this.weblink.wxGoWebPay(this.storage.get('token'), data.data.datas.orderNo, data.data.datas.realpay, data.data.datas.orderType);
         });
       }
     });
